8b7885ea8a28e50f55a2583f4a34f9ffc8476d54
crmcustoms <info@crmcustoms.com>
2026-09-02 15:21:00 +0300

    Прибрано зайву копію lendos з репо (живий окремо на crmcustoms.github.io/lendos)

--- public/works/lendos/_next/static/chunks/app/success/page-8bd45f17e127daa6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[582],{5734:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2663,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[663,347,233,358],()=>s(5734)),_N_E=e.O()}]);
